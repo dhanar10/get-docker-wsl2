6aae6fd299a971c0374b2e52a298c77eced76916
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2021-11-25 05:15:22 +0700

    feat: add distribution param

--- get-docker-wsl2.ps1
+++ get-docker-wsl2.ps1
@@ -1,18 +1,22 @@
+Param (
+	[string]$distribution = 'Ubuntu-20.04'
+)
+
 # https://docs.docker.com/engine/install/ubuntu/#install-using-the-convenience-script
 
-wsl -d Ubuntu-20.04 -u root bash -c @'
+wsl -d $distribution -u root bash -c @'
 (TMP_DIR=$(mktemp -d) && curl -fsSL https://get.docker.com -o $TMP_DIR/get-docker.sh && sudo sh $TMP_DIR/get-docker.sh); rm -rfv $TMP_DIR
 '@
 
 # https://stackoverflow.com/a/64073035
 
 $wshShell = New-Object -comObject WScript.Shell
-$shortcut = $wshShell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\wsl2-docker-ubuntu-20.04.lnk")
+$shortcut = $wshShell.CreateShortcut("$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\wsl2-docker-$($distribution.ToLower()).lnk")
 $shortcut.TargetPath = "C:\Windows\System32\wsl.exe"
-$shortcut.Arguments = "-d Ubuntu-20.04 -u root bash -c 'service docker start'"
+$shortcut.Arguments = "-d $distribution -u root bash -c 'service docker start'"
 $shortcut.WindowStyle = 7   # Minimized
 $shortcut.Save()
 
 # Start docker service immediately
 
-wsl -d Ubuntu-20.04 -u root bash -c 'service docker start'
+wsl -d $distribution -u root bash -c 'service docker start'
